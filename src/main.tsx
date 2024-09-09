1f85b1406263512113d44091d7f591cc3723825d
Matias <mmatidelga2@gmail.com>
2024-09-09 12:15:02 -0300

    feat: activate user page

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import Providers from "./Providers.tsx";
 import "./styles/globals.css";
+import AppProvider from "./providers/AppProvider.tsx";
 
-createRoot(document.getElementById("root")!).render(<Providers />);
+createRoot(document.getElementById("root")!).render(<AppProvider />);
