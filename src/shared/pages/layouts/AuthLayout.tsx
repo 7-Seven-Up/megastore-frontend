e0a5f15a57235618b2a31b132908b05f0fdf4513
Matias <mmatidelga2@gmail.com>
2024-09-09 17:04:52 -0300

    feat: add logo to auth pages

--- src/shared/pages/layouts/AuthLayout.tsx
+++ src/shared/pages/layouts/AuthLayout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Logo from "@/shared/components/ui/Logo.tsx";
 
 export default function AuthLayout() {
   return (
     <main className={"flex min-h-screen"}>
       <section
-        className={"hidden min-h-screen flex-1 bg-primary lg:block"}
-      ></section>
+        className={
+          "hidden min-h-screen flex-1 items-center justify-center bg-[#053262] lg:flex"
+        }
+      >
+        <Logo />
+      </section>
       <section className={"flex flex-1 items-center justify-center"}>
         <Outlet />
       </section>
