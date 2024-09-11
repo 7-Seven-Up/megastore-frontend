479e8053cc213a3ef5fbd7b95aef90cc508c4b30
Matias <mmatidelga2@gmail.com>
2024-09-11 14:17:57 -0300

    ref: form validations to match backend requirements

--- src/modules/auth/schemas/sign-up.schema.ts
+++ src/modules/auth/schemas/sign-up.schema.ts
@@ -5,32 +5,44 @@ export const SignUpSchema = z.object({
     .string({
       required_error: "",
     })
-    .email({
-      message: "El email no es valido",
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: "E-mail format is not valid",
     }),
   fullName: z
     .string({
       required_error: "",
     })
-    .min(3)
-    .max(20),
+    .min(3, {
+      message: "Full name must be at least 3 characters long",
+    })
+    .max(20, {
+      message: "Full name must be at most 20 characters long",
+    }),
   password: z
     .string({
       required_error: "",
     })
-    .min(3)
-    .max(20),
+    .min(6, {
+      message: "Password must be at least 6 characters long",
+    })
+    .max(20, {
+      message: "Password must be at most 20 characters long",
+    }),
   phoneNumber: z
     .string({
       required_error: "",
     })
-    .regex(/^\d{10}$/, { message: "Numero de teléfono invalido" }),
+    .regex(/^\d{10}$/, { message: "Phone number is not valid" }),
   username: z
     .string({
       required_error: "",
     })
-    .min(3)
-    .max(20),
+    .min(3, {
+      message: "Username must be at least 3 characters long",
+    })
+    .max(20, {
+      message: "Username must be at most 20 characters long",
+    }),
 });
 
 export type SignUpSchemaType = z.infer<typeof SignUpSchema>;
