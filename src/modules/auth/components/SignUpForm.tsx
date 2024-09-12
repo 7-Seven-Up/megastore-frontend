485d6717650340a0b9af7505c431bcc80a4b2ad5
Matias <mmatidelga2@gmail.com>
2024-09-12 19:03:33 -0300

    ref: reformat imports

--- src/modules/auth/components/SignUpForm.tsx
+++ src/modules/auth/components/SignUpForm.tsx
@@ -1,11 +1,14 @@
-import {Button} from "@nextui-org/react";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { Button } from "@nextui-org/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import {InputField} from "@/shared/components/ui/InputField.tsx";
-import {PasswordInput} from "@/shared/components/ui/PasswordInput.tsx";
-import {SignUpSchema, SignUpSchemaType,} from "@/modules/auth/schemas/sign-up.schema.ts";
-import {Title} from "@/shared/components/typography/Title.tsx";
+import { InputField } from "@/shared/components/ui/InputField.tsx";
+import { PasswordInput } from "@/shared/components/ui/PasswordInput.tsx";
+import {
+  SignUpSchema,
+  SignUpSchemaType,
+} from "@/modules/auth/schemas/sign-up.schema.ts";
+import { Title } from "@/shared/components/typography/Title.tsx";
 
 export function SignUpForm() {
   const { control, handleSubmit } = useForm<SignUpSchemaType>({
