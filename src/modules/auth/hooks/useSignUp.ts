4abde98a1a8ca869a7601912444fcfbee7805265
Matías Delgado <mmatidelga2@gmail.com>
2024-09-12 20:25:14 -0300

    Update useSignUp.ts

--- src/modules/auth/hooks/useSignUp.ts
+++ src/modules/auth/hooks/useSignUp.ts
@@ -7,7 +7,7 @@ import { signUpUser } from "@auth/auth.service.ts";
 export function useSignUp() {
   const navigate = useNavigate();
   const { isError, isPending, mutate } = useMutation({
-    mutationKey: [AUTH_SIGN_IN_KEY],
+    mutationKey: [AUTH_SIGN_UP_KEY],
     mutationFn: signUpUser,
     onSuccess: () => {
       navigate("/auth/email-sent", { replace: true });
