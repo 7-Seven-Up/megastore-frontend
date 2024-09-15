81efededbb2cedc5ac295959f57ecce658bbd26b
Matias <mmatidelga2@gmail.com>
2024-09-14 21:46:26 -0300

    feat: authenticate users

--- src/modules/auth/constants.ts
+++ src/modules/auth/constants.ts
@@ -1,1 +1,2 @@
+export const AUTH_SIGN_UP_KEY = "auth/sign-up";
 export const AUTH_SIGN_IN_KEY = "auth/sign-in";
--- src/shared/lib/httpClient.ts
+++ src/shared/lib/httpClient.ts
@@ -10,7 +10,31 @@ export const httpClient = axios.create({
 httpClient.interceptors.response.use(
   (response) => response,
   (error: AxiosError<ErrorResponse>) => {
+    if (error.status === 403 || error.status === 401) {
+      toast.error("You are not authorized to access this resource.");
+      return Promise.reject(error);
+    }
+
     toast.error(error.response?.data.message ?? "An unknown error occurred.");
     return Promise.reject(error);
   },
 );
+
+httpClient.interceptors.request.use(function (config) {
+  const excludedPaths = ["/auth/signin", "/auth/signup"];
+  const shouldExclude = excludedPaths.some((path) =>
+    config.url?.includes(path),
+  );
+
+  if (shouldExclude) {
+    return config;
+  }
+
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (accessToken) {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
