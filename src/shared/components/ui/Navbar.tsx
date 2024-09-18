760707ee7f044f776365055464263c50b5c4b3e1
Matias <mmatidelga2@gmail.com>
2024-09-18 16:53:49 -0300

    ref: show link to admin panel in navbar

--- src/shared/components/ui/Navbar.tsx
+++ src/shared/components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Divider,
   Link,
   Navbar as NextNavbar,
   NavbarBrand,
@@ -13,6 +14,9 @@ import { useLocation } from "react-router-dom";
 import { Title } from "@/shared/components/typography/Title.tsx";
 import { useAuthStore } from "@auth/hooks/useAuthStore.ts";
 import RenderIf from "@/shared/components/RenderIf.tsx";
+import { LayoutIcon } from "@/shared/components/icons/LayoutIcon.tsx";
+import { RoleBasedVisibility } from "@/shared/components/ui/RoleBasedVisibility.tsx";
+import { Role } from "@/modules/users/enums/role.enum.ts";
 
 export function Navbar() {
   const { isAuthenticated, logout } = useAuthStore();
@@ -33,15 +37,17 @@ export function Navbar() {
       disableAnimation
       isBlurred
       maxWidth={"full"}
-      className={"bg-background py-3"}
+      className={
+        "border-1 border-r-0 border-t-0 border-b-default/50 bg-background py-3"
+      }
     >
-      <NavbarContent className="pr-3 sm:hidden" justify="center">
+      <NavbarContent className="pr-3 lg:hidden" justify="center">
         <NavbarBrand>
           <Title className={"text-3xl"}>Megastore</Title>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+      <NavbarContent className="hidden gap-4 lg:flex" justify="center">
         <NavbarBrand className={"pr-4"}>
           <Title>Megastore</Title>
         </NavbarBrand>
@@ -56,33 +62,20 @@ export function Navbar() {
         })}
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden" justify="end">
-        <div className={"pr-2"}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="#000000"
-            viewBox="0 0 256 256"
-          >
-            <path d="M230.14,58.87A8,8,0,0,0,224,56H62.68L56.6,22.57A8,8,0,0,0,48.73,16H24a8,8,0,0,0,0,16h18L67.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,160,204a28,28,0,1,0,28-28H91.17a8,8,0,0,1-7.87-6.57L80.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,230.14,58.87ZM104,204a12,12,0,1,1-12-12A12,12,0,0,1,104,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,200,204Zm4-74.57A8,8,0,0,1,196.1,136H77.22L65.59,72H214.41Z"></path>
-          </svg>
-        </div>
+      {/* Content for mobile */}
+      <NavbarContent className="lg:hidden" justify="end">
+        <div className={"pr-2"}></div>
         <NavbarMenuToggle />
       </NavbarContent>
 
+      {/* Content for desktop */}
       <NavbarContent justify="end" className={"hidden lg:flex"}>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            fill="#000000"
-            viewBox="0 0 256 256"
-          >
-            <path d="M230.14,58.87A8,8,0,0,0,224,56H62.68L56.6,22.57A8,8,0,0,0,48.73,16H24a8,8,0,0,0,0,16h18L67.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,160,204a28,28,0,1,0,28-28H91.17a8,8,0,0,1-7.87-6.57L80.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,230.14,58.87ZM104,204a12,12,0,1,1-12-12A12,12,0,0,1,104,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,200,204Zm4-74.57A8,8,0,0,1,196.1,136H77.22L65.59,72H214.41Z"></path>
-          </svg>
-        </div>
+        <RoleBasedVisibility allowedRoles={[Role.ADMIN]}>
+          <Link href={"/admin"}>
+            <LayoutIcon />
+          </Link>
+          <Divider orientation={"vertical"} />
+        </RoleBasedVisibility>
 
         <RenderIf condition={!isAuthenticated}>
           <NavbarItem>
@@ -122,6 +115,29 @@ export function Navbar() {
               </NavbarMenuItem>
             );
           })}
+        </div>
+
+        <div className={"flex flex-col gap-2"}>
+          <RoleBasedVisibility allowedRoles={[Role.ADMIN]}>
+            <NavbarMenuItem isActive={pathname === "/admin"}>
+              <Link href={"/admin"} color={"foreground"}>
+                Admin panel
+              </Link>
+            </NavbarMenuItem>
+          </RoleBasedVisibility>
+
+          <RenderIf condition={isAuthenticated}>
+            <NavbarMenuItem>
+              <Button
+                onClick={logout}
+                color="secondary"
+                variant="flat"
+                className={"w-full"}
+              >
+                Logout
+              </Button>
+            </NavbarMenuItem>
+          </RenderIf>
         </div>
 
         <RenderIf condition={!isAuthenticated}>
