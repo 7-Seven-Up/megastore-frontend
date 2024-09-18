a97bea74a0d19fef27b3766a94498a83fafc8038
Matias <mmatidelga2@gmail.com>
2024-09-18 16:55:40 -0300

    style: create custom variables

--- src/shared/pages/layouts/AppLayout.tsx
+++ src/shared/pages/layouts/AppLayout.tsx
@@ -7,7 +7,7 @@ export function AppLayout() {
       <Navbar />
       <main
         className={
-          "mx-auto flex min-h-[calc(100dvh_-_88px)] max-w-screen-2xl items-center justify-center p-6"
+          "min-h-screenMinusNavbar mx-auto flex items-center justify-center"
         }
       >
         <Outlet />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ export default {
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
       },
+      spacing: {
+        "admin-sidebar": "var(--admin-sidebar-width)",
+        screenMinusNavbar: "calc(100dvh - 90px)",
+      },
     },
   },
   plugins: [nextui()],
