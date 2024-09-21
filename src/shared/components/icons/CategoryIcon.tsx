35781ef854cb29aa06d90e2ff79308b5536fec27
Matias <mmatidelga2@gmail.com>
2024-09-20 23:41:30 -0300

    ref: icons on admin aside

---
+++ src/shared/components/icons/CategoryIcon.tsx
@@ -0,0 +1,36 @@
+export function CategoryIcon() {
+  return (
+    <svg
+      className={"size-5 lg:size-7"}
+      fill="none"
+      stroke="#808080"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M10 3h-6a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1 -1v-6a1 1 0 0 0 -1 -1z"
+        stroke-width="0"
+        fill="#53535B"
+      />
+      <path
+        d="M20 3h-6a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1 -1v-6a1 1 0 0 0 -1 -1z"
+        stroke-width="0"
+        fill="#53535B"
+      />
+      <path
+        d="M10 13h-6a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1 -1v-6a1 1 0 0 0 -1 -1z"
+        stroke-width="0"
+        fill="#53535B"
+      />
+      <path
+        d="M17 13a4 4 0 1 1 -3.995 4.2l-.005 -.2l.005 -.2a4 4 0 0 1 3.995 -3.8z"
+        stroke-width="0"
+        fill="#53535B"
+      />
+    </svg>
+  );
+}
