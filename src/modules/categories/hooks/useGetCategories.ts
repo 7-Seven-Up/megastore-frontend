b3cc6530dec72156cd687de83d47c83c3a5476af
Matias <mmatidelga2@gmail.com>
2024-09-24 13:45:09 -0300

    ref: create local variable of fixedPage

--- src/modules/categories/hooks/useGetCategories.ts
+++ src/modules/categories/hooks/useGetCategories.ts
@@ -5,14 +5,16 @@ import { getAllCategories } from "@/modules/categories/categories.service.ts";
 
 export function useGetCategories(params: PaginationRequest) {
   const { pageSize = 10, page = 0, ...rest } = params;
+  const fixedPage = page - 1 < 0 ? page : page - 1;
+
   const { data, isLoading, isError } = useQuery({
     queryFn: () =>
       getAllCategories({
-        page: page - 1 < 0 ? page : page - 1,
+        page: fixedPage,
         pageSize,
         ...rest,
       }),
-    queryKey: [GET_CATEGORIES_KEY, page, pageSize],
+    queryKey: [GET_CATEGORIES_KEY, fixedPage, pageSize],
     staleTime: Infinity,
   });
 
