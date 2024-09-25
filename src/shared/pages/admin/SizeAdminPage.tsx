e7cb394b4b922340432ddcebb22baf83acb8ed96
EzeSosa <ezequielsosa423@gmail.com>
2024-09-25 01:59:59 -0300

    feat: sizes table

--- src/shared/pages/admin/SizeAdminPage.tsx
+++ src/shared/pages/admin/SizeAdminPage.tsx
@@ -2,6 +2,7 @@ import { Button, Link } from "@nextui-org/react";
 import { PlusIcon } from "@/shared/components/icons/PlusIcon.tsx";
 import { Title } from "@/shared/components/typography/Title.tsx";
 import { Outlet } from "react-router-dom";
+import { SizesTable } from "@/modules/sizes/components/SizesTable";
 
 export function SizeAdminPage() {
   return (
@@ -20,7 +21,7 @@ export function SizeAdminPage() {
           Create new
         </Button>
       </header>
-      {/* <SizesTable /> */}
+      <SizesTable />
       <Outlet />
     </div>
   );
