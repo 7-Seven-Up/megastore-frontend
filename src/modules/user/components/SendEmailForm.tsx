1cd2428f7887df0300f047ea2be6ec454fce7679
EzeSosa <ezequielsosa423@gmail.com>
2024-09-27 13:31:07 -0300

    ref: remove unnecessary divs and use global zod form

--- src/modules/user/components/SendEmailForm.tsx
+++ src/modules/user/components/SendEmailForm.tsx
@@ -4,20 +4,17 @@ import {
   SendEmailSchemaType,
   SendEmailSchema,
 } from "@/modules/user/schemas/send-email.schema.ts";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useSendEmail } from "@/modules/user/hooks/useSendEmail.ts";
 import { Title } from "@/shared/components/typography/Title";
 import { PATH_TO_NAVIGATE_MAP } from "../route-constants";
+import { useZodForm } from "@/shared/hooks/useZodForm.ts";
 
 interface SendEmailFormProps {
   path: string;
 }
 
 export default function SendEmailForm({ path }: SendEmailFormProps) {
-  const { control, handleSubmit } = useForm<SendEmailSchemaType>({
-    resolver: zodResolver(SendEmailSchema),
-  });
+  const { control, handleSubmit } = useZodForm(SendEmailSchema);
 
   const navigate_to: string = PATH_TO_NAVIGATE_MAP[path] || "/";
   const { mutate, isPending } = useSendEmail({ navigate_to });
@@ -31,20 +28,16 @@ export default function SendEmailForm({ path }: SendEmailFormProps) {
       className={"flex w-full flex-col gap-4"}
       onSubmit={handleSubmit(onSubmit)}
     >
-      <div className={"grid gap-4"}>
-        <Title>Send email</Title>
-      </div>
+      <Title>Send email</Title>
 
-      <div className={"flex gap-4"}>
-        <InputField<SendEmailSchemaType>
-          className={"col-span-12 lg:col-span-6"}
-          control={control}
-          label={"Email"}
-          name={"email"}
-          placeholder={"Type your email"}
-          type={"text"}
-        />
-      </div>
+      <InputField<SendEmailSchemaType>
+        className={"col-span-12 lg:col-span-6"}
+        control={control}
+        label={"Email"}
+        name={"email"}
+        placeholder={"Type your email"}
+        type={"text"}
+      />
       <Button color={"secondary"} isLoading={isPending} type={"submit"}>
         Send
       </Button>
