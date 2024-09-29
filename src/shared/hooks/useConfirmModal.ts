1b8fe567c6d81ab3847a8a6374d0ccab0a1b91a0
Matias <mmatidelga2@gmail.com>
2024-09-28 23:23:55 -0300

    feat: global confirm modal

---
+++ src/shared/hooks/useConfirmModal.ts
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+
+export type ConfirmModalProps = {
+  description: string;
+  onConfirm: () => Promise<void>;
+  title: string;
+};
+
+type ConfirmModalStore = {
+  description: string;
+  isLoading: boolean;
+  modalVisible: boolean;
+  onConfirm: () => Promise<void>;
+  title: string;
+  hideConfirmModal: () => void;
+  setIsLoading?: (value: boolean) => void;
+  showConfirmModal: (props: ConfirmModalProps) => void;
+};
+
+const INITIAL_STATE = {
+  description: "",
+  isLoading: false,
+  modalVisible: false,
+  onConfirm: async () => {},
+  title: "",
+};
+
+export const useConfirmModal = create<ConfirmModalStore>((set) => ({
+  ...INITIAL_STATE,
+  showConfirmModal: (props) => {
+    set({
+      ...props,
+      modalVisible: true,
+      onConfirm: async () => {
+        set({ isLoading: true });
+        await props.onConfirm();
+        set({ isLoading: false, modalVisible: false });
+      },
+    });
+  },
+
+  hideConfirmModal: () => {
+    set({
+      ...INITIAL_STATE,
+    });
+  },
+}));
