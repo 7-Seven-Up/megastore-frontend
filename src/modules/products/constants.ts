652bd1bcb2120b5664761faf140f786827fb775e
Matias <mmatidelga2@gmail.com>
2024-09-29 00:15:04 -0300

    ref: duplicated get products key

--- src/modules/products/constants.ts
+++ src/modules/products/constants.ts
@@ -1,4 +1,3 @@
 export const GET_PRODUCTS_KEY = "products/get";
 export const CREATE_PRODUCTS_KEY = "products/create";
-export const GET_PRODUCTS_KEY = "products/get";
 export const DELETE_PRODUCTS_KEY = "products/delete";
