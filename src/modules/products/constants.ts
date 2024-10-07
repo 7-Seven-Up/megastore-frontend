ba81eab9e070d09bda24a4538a6d93cdafdbb9ed
Ezequiel Sosa <87153869+EzeSosa@users.noreply.github.com>
2024-10-07 16:39:00 -0300

    fix: max image size of 1MB (#49)

--- src/modules/products/constants.ts
+++ src/modules/products/constants.ts
@@ -1,3 +1,4 @@
 export const GET_PRODUCTS_KEY = "products/get";
 export const CREATE_PRODUCTS_KEY = "products/create";
 export const DELETE_PRODUCTS_KEY = "products/delete";
+export const MAXIMUM_IMAGE_SIZE = 1024 * 1024;
