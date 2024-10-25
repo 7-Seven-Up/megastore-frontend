c3a0fdfb2263eff98d3159d96b965fcfbd345fae
Matias <mmatidelga2@gmail.com>
2024-10-25 10:54:49 -0300

    chore: update path alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@auth": path.resolve(__dirname, "./src/modules/auth"),
       "@products": path.resolve(__dirname, "./src/modules/products"),
-      "@user": path.resolve(__dirname, "./src/modules/user"),
+      "@user": path.resolve(__dirname, "./src/modules/users"),
+      "@sizes": path.resolve(__dirname, "./src/modules/sizes"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
 });
