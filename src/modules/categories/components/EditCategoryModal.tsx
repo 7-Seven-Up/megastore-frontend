f2b6790d79a1f5d7a01d028fcb3000069d94de27
Matias <mmatidelga2@gmail.com>
2024-10-27 22:56:31 -0300

    ref: edit category modal component

---
+++ src/modules/categories/components/EditCategoryModal.tsx
@@ -0,0 +1,25 @@
+import { Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react";
+
+import { Category } from "@categories/interfaces/responses/category.interface.ts";
+import { EditCategoryForm } from "@categories/components/EditCategoryForm.tsx";
+
+interface EditCategoryModalProps {
+  editingCategory: Category;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export function EditCategoryModal(props: EditCategoryModalProps) {
+  const { editingCategory, isOpen, onClose } = props;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} backdrop={"blur"} size={"lg"}>
+      <ModalContent>
+        <ModalHeader>Edit category</ModalHeader>
+        <ModalBody>
+          <EditCategoryForm category={editingCategory} onClose={onClose} />
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
