c0d4e54c8361992a0a72c5f96fe2ceb0924076a2
Matias <mmatidelga2@gmail.com>
2024-10-27 22:54:10 -0300

    ref: allow default selection in select inputs

--- src/modules/categories/components/CategoriesSelect.tsx
+++ src/modules/categories/components/CategoriesSelect.tsx
@@ -1,15 +1,23 @@
 import { useMemo, useState } from "react";
+import { FieldValues } from "react-hook-form";
+
 import { SelectItem } from "@nextui-org/react";
 import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
-import { FieldValues } from "react-hook-form";
+
+import { Category } from "@categories/interfaces/responses/category.interface.ts";
 import { ControlledSelect } from "@/shared/components/ui/ControlledSelect.tsx";
-import { useInfiniteCategories } from "@/modules/categories/hooks/useInfiniteCategories.ts";
 import { Select } from "@/shared/interfaces/select.interface.ts";
+import { useInfiniteCategories } from "@/modules/categories/hooks/useInfiniteCategories.ts";
+
+type CategoriesSelectProps<T extends FieldValues> = Select<T> & {
+  defaultSelected?: Category;
+};
 
 export function CategoriesSelect<T extends FieldValues>({
   control,
+  defaultSelected,
   ...rest
-}: Select<T>) {
+}: CategoriesSelectProps<T>) {
   const { data, fetchNextPage, isLoading, hasNextPage } =
     useInfiniteCategories();
 
@@ -22,8 +30,22 @@ export function CategoriesSelect<T extends FieldValues>({
   });
 
   const categories = useMemo(() => {
-    return data?.pages.map((page) => page.content).flat();
-  }, [data]);
+    const flatCategories = data?.pages.map((page) => page.content).flat();
+
+    if (defaultSelected && flatCategories) {
+      return [
+        {
+          categoryId: defaultSelected.categoryId,
+          name: defaultSelected.name,
+        },
+        ...flatCategories.filter(
+          (item) => item.categoryId !== defaultSelected.categoryId,
+        ),
+      ];
+    }
+
+    return flatCategories;
+  }, [defaultSelected, data]);
 
   return (
     <ControlledSelect
--- src/modules/sizes/components/SizesSelect.tsx
+++ src/modules/sizes/components/SizesSelect.tsx
@@ -1,15 +1,23 @@
-import { FieldValues } from "react-hook-form";
-import { Select } from "@/shared/interfaces/select.interface.ts";
 import { useMemo, useState } from "react";
-import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
-import { ControlledSelect } from "@/shared/components/ui/ControlledSelect.tsx";
+import { FieldValues } from "react-hook-form";
+
 import { SelectItem } from "@nextui-org/react";
+import { useInfiniteScroll } from "@nextui-org/use-infinite-scroll";
+
+import { ControlledSelect } from "@/shared/components/ui/ControlledSelect.tsx";
+import { Select } from "@/shared/interfaces/select.interface.ts";
+import { Size } from "@sizes/interfaces/responses/size.interface.ts";
 import { useInfiniteSizes } from "@/modules/sizes/hooks/useInfiniteSizes.ts";
+
+type SizesSelectProps<T extends FieldValues> = Select<T> & {
+  defaultSelected?: Size;
+};
 
 export function SizesSelect<T extends FieldValues>({
   control,
+  defaultSelected,
   ...rest
-}: Select<T>) {
+}: SizesSelectProps<T>) {
   const { data, fetchNextPage, isLoading, hasNextPage } = useInfiniteSizes();
 
   const [isOpen, setIsOpen] = useState(false);
@@ -20,15 +28,24 @@ export function SizesSelect<T extends FieldValues>({
     shouldUseLoader: false,
   });
 
-  const categories = useMemo(() => {
-    return data?.pages.map((page) => page.content).flat();
-  }, [data]);
+  const sizes = useMemo(() => {
+    const flatSizes = data?.pages.map((page) => page.content).flat();
+
+    if (defaultSelected && flatSizes) {
+      return [
+        defaultSelected,
+        ...flatSizes.filter((size) => size.sizeId !== defaultSelected.sizeId),
+      ];
+    }
+
+    return flatSizes;
+  }, [defaultSelected, data]);
 
   return (
     <ControlledSelect
       control={control}
       isLoading={isLoading}
-      items={categories || []}
+      items={sizes || []}
       label={"Super category"}
       name={"superCategoryId"}
       onOpenChange={setIsOpen}
