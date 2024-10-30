9b8866c4f73e4466e208080db974b4126f5b68a8
EzeSosa <ezequielsosa423@gmail.com>
2024-10-30 17:56:04 -0300

    ref: keep same signup validations

--- src/modules/users/schemas/recover-password.schema.ts
+++ src/modules/users/schemas/recover-password.schema.ts
@@ -8,19 +8,6 @@ export const RecoverPasswordSchema = z
       })
       .min(6, {
         message: "Password must be at least 6 characters long",
-      })
-      .max(20, {
-        message: "Password must be at most 20 characters long",
-      })
-      .regex(/[A-Z]/, {
-        message: "Password must contain at least one uppercase letter",
-      })
-      .regex(/[a-z]/, {
-        message: "Password must contain at least one lowercase letter",
-      })
-      .regex(/[0-9]/, { message: "Password must contain at least one number" })
-      .regex(/[\W_]/, {
-        message: "Password must contain at least one special character",
       }),
     confirmPassword: z.string({
       required_error: "",
