cd076e0e6a5b2bde7ec4025f0b8cb718855a2b86
Ezequiel Sosa <87153869+EzeSosa@users.noreply.github.com>
2024-10-30 21:32:59 -0300

    ref: password token (#52)
    * feat: add toast on recover password success
    
    * ref: keep same signup validations

--- src/modules/users/hooks/useRecoverPassword.ts
+++ src/modules/users/hooks/useRecoverPassword.ts
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { recoverPassword } from "@/modules/users/user.service.ts";
+import { toast } from "sonner";
 
 export const useRecoverPassword = () => {
   const navigate = useNavigate();
@@ -8,6 +9,7 @@ export const useRecoverPassword = () => {
   const { isError, isPending, mutate } = useMutation({
     mutationFn: recoverPassword,
     onSuccess: () => {
+      toast.success("Password recovered successfully", { duration: 3000 });
       navigate("/auth/signin", { replace: true });
     },
   });
